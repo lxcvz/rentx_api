fd9c06ceccd30d33e8c81aa60117e40de830ee6a
Lucas <lucas-mateus.dc@hotmail.com>
2022-02-01 22:04:04 -0300

    removed username column from users table

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -12,7 +12,6 @@ class UsersRepository implements IUsersRepository {
 
     async create({
         name,
-        username,
         password,
         email,
         driver_license
@@ -20,7 +19,6 @@ class UsersRepository implements IUsersRepository {
 
         const user = this.repository.create({
             name,
-            username,
             password,
             email,
             driver_license
